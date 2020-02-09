87f9b8a488127ec661dc6e9cb8b6973ffb9cf8c4
yuseon27 <ysnam19@naver.com>
2020-02-09 16:13:58 +0900

    Start Model Project - Put model class into DB

---
+++ django/03. Model/Model/ModelApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model) : #{
+    title    = models.CharField(max_length=200)
+    pub_data = models.DateTimeField('data published')
+    body     = models.TextField()
+
+    def __str__(self) : #{
+        return self.title    
+    #}
+#}
