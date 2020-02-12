554247abdabeba8d5043440aa244f12c422e113e
yuseon27 <ysnam19@naver.com>
2020-02-12 17:28:13 +0900

    Finished Model Project - linking db and project

--- django/03. Model/Model/ModelApp/views.py
+++ django/03. Model/Model/ModelApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def home(request) : #{
+    blogs = Blog.objects     # Query Set // Method : deal with functions of query set
+
+    return render(request, 'home.html', {'blogs':blogs})   
+#}
--- django/03. Model/Model/ModelProject/urls.py
+++ django/03. Model/Model/ModelProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import ModelApp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ModelApp.views.home, name="home"),
 ]
