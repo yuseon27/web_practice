9585c82985aef238a335601f916eefb6bcc50597
yuseon27 <ysnam19@naver.com>
2020-02-18 17:12:17 +0900

    Blog Using Bootstrap has started

---
+++ django/05. Blog Bootstrap/BlogBootstrapProject/BlogApp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model) : #{
+    title    = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Date Published')
+    body     = models.TextField()
+
+    def __str__(self) : #{
+        return self.title    
+    #}
+
+    def preview(self) : #{
+        return self.body[:100]    
+    #}
+#}
---
+++ django/05. Blog Bootstrap/BlogBootstrapProject/BlogApp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+# Create your views here.
+def home(reqeust) : #{
+    blog_all = Blog.objects
+
+    return render(reqeust, 'home.html', {'blog_all':blog_all})
+#}
+
+def detail(request, blog_id) : #{
+    blog_detail = get_object_or_404(Blog, pk=blog_id)    ## FREMEBER to write pk=
+
+    return render(request, 'detail.html', {'blog_detail':blog_detail})
+#}
