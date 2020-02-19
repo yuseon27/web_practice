40161515534f8391dfb711957b371c3d74bf5d55
yuseon27 <ysnam19@naver.com>
2020-02-19 17:52:10 +0900

    Added Design in blog bootstrap project

--- django/05. Blog Bootstrap/BlogBootstrapProject/BlogApp/models.py
+++ django/05. Blog Bootstrap/BlogBootstrapProject/BlogApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Blog(models.Model) : #{
+class Blog(models.Model) : #{   ## REMEBER TO WRITE models.Model
     title    = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Date Published')
     body     = models.TextField()
